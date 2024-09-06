fe8e2a77bf8fb01280598485d52cfa39d867fe49
Juniinoficial <93329770+Juniinoficial@users.noreply.github.com>
2024-09-06 17:50:45 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros com sua determinação e talento. Bicampeã olímpica, ela se tornou a maior medalhista olímpica da história do Brasil, superando marcas históricas. Sua trajetória, marcada por superação e dedicação, a tornou um símbolo de inspiração para jovens atletas e para todo o país. Com suas acrobacias impressionantes e sua personalidade carismática, Rebeca Andrade é, sem dúvida, uma das maiores atletas brasileiras de todos os tempos.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica artística bicampeã olímpica inspiração acrobacias determinação talento"
+    },
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, conhecida como Fadinha, é uma skatista profissional brasileira. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, conquistando a prata nos Jogos Olímpicos de Tóquio 2020. Sua habilidade e carisma a tornaram um ícone do skate mundial.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate Fadinha medalhista olímpica jovem talento ícone carisma"
+    },
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira. Aos 24 anos, conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024, na categoria +78kg. Sua vitória histórica elevou o judô feminino brasileiro a um novo patamar.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô ouro olímpico judô feminino histórica força dedicação"
+    }
+];
